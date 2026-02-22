34253d9239475c32b512ddc9e7e637ddf4fa7bf0
Inoue-KK <kyo.yuzukko.99@gmail.com>
2026-02-22 21:04:27 +0900

    feat: add day of week header to Calendar

--- app/components/Calendar/Calendar.tsx
+++ app/components/Calendar/Calendar.tsx
@@ -1,15 +1,23 @@
 'use client';
 
+import { getDaysInMonth, getFirstDayOfMonth } from "@/app/utils/date";
 import { useState } from "react";
 
 export default function Calendar() {
     const today = new Date();
     const [year, setYear] = useState(today.getFullYear());
     const [month, setMonth] = useState(today.getMonth() + 1);
+    const daysInMonth = getDaysInMonth(year, month);
+    const firstDayOfWeek = getFirstDayOfMonth(year, month);
 
     return (
         <div>
             <h1>{year}年{month}月</h1>
+            <div>
+                {['日', '月', '火', '水', '木', '金', '土'].map((day) => (
+                    <span key={day}>{day}</span>
+                ))}
+            </div>
         </div>
     );
 }
